677e3545a9a29a123b565267d347e698da930a99
kushurox <kushalsai2449@gmail.com>
2025-05-05 15:43:15 +0530

    got the baby to work

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 #![no_main]
 #![no_std]
 
+use cortex_m::{delay::Delay, Peripherals};
 use cortex_m_rt::entry;
 use cortex_m_semihosting::hprintln;
 use panic_halt as _;
@@ -22,6 +23,10 @@ fn kmain() -> ! {
 
     let ep_mem = unsafe { EP_MEMORY.init([0; 1024]) };
     let dp = pac::Peripherals::take().unwrap();
+    let cp = Peripherals::take().unwrap();
+
+    let syst = cp.SYST;
+    let mut d = Delay::new(syst, 48_000_000);
 
     let rcc = dp.RCC.constrain();
     let clocks = rcc.cfgr
@@ -30,6 +35,7 @@ fn kmain() -> ! {
     .pclk1(24.MHz())
     .require_pll48clk()
     .freeze();
+
 
     let gpiob = dp.GPIOB.split();
 
@@ -40,48 +46,63 @@ fn kmain() -> ! {
     let mut pca = PCA9685::new(i2c);
 
     let _ = pca.set_pwm_frequency(50.Hz());
-    let _ = pca.set_duty_cycle(0, 0.3);
-    let _ = pca.set_duty_cycle(1, 0.5);
+    pca.set_duty_cycle(4, degrees_to_ds(0f32));
+    d.delay_ms(3000);
+    pca.set_duty_cycle(4, degrees_to_ds(40f32));
+    // let mut ds = 0f32;
+    // let mut inc = 0.01;
 
-    let gpioa = dp.GPIOA.split();
-    let usb_dm = gpioa.pa11.into_alternate();
-    let usb_dp = gpioa.pa12.into_alternate();
+    // loop {
+    //     ds += inc;
+    //     if ds >= 0.2 {inc = -0.01}
+    //     else if ds <= 0.0{
+    //         inc = 0.01
+    //     }
+    //     let _ = pca.set_duty_cycle(4, ds);
+    //     let _ = pca.set_duty_cycle(0, ds);
+    //     d.delay_ms(100);
 
-    // Set up USB peripheral
-    let usb = USB {
-        usb_global: dp.OTG_FS_GLOBAL,
-        usb_device: dp.OTG_FS_DEVICE,
-        usb_pwrclk: dp.OTG_FS_PWRCLK,
-        pin_dm: usb_dm.into(),
-        pin_dp: usb_dp.into(),
-        hclk: clocks.hclk()
-    };
+    // }
 
-    // Create USB bus and serial port
-    let usb_bus = UsbBus::new(usb, ep_mem);
-    let mut serial = SerialPort::new(&usb_bus);
-    let mut usb_dev = UsbDeviceBuilder::new(&usb_bus, UsbVidPid(0x16c0, 0x27dd))
-    .strings(&[StringDescriptors::default()
-    .manufacturer("kushurox")
-    .product("totm")
-    .serial_number("TEST")
-    ]).unwrap()
-    .device_class(USB_CLASS_CDC).build();
+    // let gpioa = dp.GPIOA.split();
+    // let usb_dm = gpioa.pa11.into_alternate();
+    // let usb_dp = gpioa.pa12.into_alternate();
+
+    // // Set up USB peripheral
+    // let usb = USB {
+    //     usb_global: dp.OTG_FS_GLOBAL,
+    //     usb_device: dp.OTG_FS_DEVICE,
+    //     usb_pwrclk: dp.OTG_FS_PWRCLK,
+    //     pin_dm: usb_dm.into(),
+    //     pin_dp: usb_dp.into(),
+    //     hclk: clocks.hclk()
+    // };
+
+    // // Create USB bus and serial port
+    // let usb_bus = UsbBus::new(usb, ep_mem);
+    // let mut serial = SerialPort::new(&usb_bus);
+    // let mut usb_dev = UsbDeviceBuilder::new(&usb_bus, UsbVidPid(0x16c0, 0x27dd))
+    // .strings(&[StringDescriptors::default()
+    // .manufacturer("kushurox")
+    // .product("totm")
+    // .serial_number("TEST")
+    // ]).unwrap()
+    // .device_class(USB_CLASS_CDC).build();
 
     loop{
-        if usb_dev.poll(&mut [&mut serial]) {
-            let mut buf = [0u8; 64];
-            if let Ok(count) = serial.read(&mut buf) {
-                hprintln!("count: {}", count);
-                if count < 16 {continue;}
-                let vals: PWMVals = buf.into();
-                hprintln!("duties: {} {} {} {}", vals.duty1, vals.duty2, vals.duty3, vals.duty4);   //  remove this when working with the real thing
-                let _ = pca.set_duty_cycle(0, vals.duty1);
-                let _ = pca.set_duty_cycle(1, vals.duty2);      // father forgive me, for I have sinned to count from 1
-                let _ = pca.set_duty_cycle(2, vals.duty3);
-                let _ = pca.set_duty_cycle(3, vals.duty4);
-            }
-        }
+        // if usb_dev.poll(&mut [&mut serial]) {
+        //     let mut buf = [0u8; 64];
+        //     if let Ok(count) = serial.read(&mut buf) {
+        //         hprintln!("count: {}", count);
+        //         if count < 16 {continue;}
+        //         let vals: PWMVals = buf.into();
+        //         hprintln!("duties: {} {} {} {}", vals.duty1, vals.duty2, vals.duty3, vals.duty4);   //  remove this when working with the real thing
+        //         let _ = pca.set_duty_cycle(0, vals.duty1);
+        //         let _ = pca.set_duty_cycle(1, vals.duty2);      // father forgive me, for I have sinned to count from 1
+        //         let _ = pca.set_duty_cycle(2, vals.duty3);
+        //         let _ = pca.set_duty_cycle(3, vals.duty4);
+        //     }
+        // }
     };
 }
 
@@ -106,4 +127,10 @@ impl From<[u8; 64]> for PWMVals {
             duty4: f32::from_le_bytes(db4)
         }
     }
+}
+
+const fn degrees_to_ds(degrees: f32) -> f32{
+    // 0.2 : 180
+    // 1: 0.2/180
+    (0.2/180f32) * degrees
 }
